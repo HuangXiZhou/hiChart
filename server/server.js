79680d22e546ac655648f14d716b8ad1d9b9d307
黄溪舟 <xizhouh@gmail.com>
2017-10-28 21:49:06 -0500

    添加Redux

--- server/server.js
+++ server/server.js
@@ -5,6 +5,41 @@ mongoose.connect(DB_URL)
 mongoose.connection.on('connected', () => {
   console.log('mongo connect success')
 })
+
+const User = mongoose.model('usrer', new mongoose.Schema({
+  user: {
+    type: String,
+    require: true
+  },
+  age: {
+    type: Number,
+    require: true
+  }
+}))
+
+// User.create({
+//   user: 'react',
+//   age: 20
+// }, (err, doc) => {
+//   if (!err) {
+//     console.log(doc)
+//   } else {
+//     console.log(err)
+//   }
+// })
+
+// User.find({}, (err, doc) => {
+//   if (!err) {
+//     console.log(doc)
+//   } else {
+//     console.log(doc)
+//   }
+// })
+
+User.remove({age:20}, (err, doc) => {
+  console.log(doc)
+})
+
 const app = express()
 
 app.listen(9093, () => {
